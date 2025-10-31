720cda8c4d597e43761d59433395b2e4a35da84e
shaofengyin <yin_shaofeng@163.com>
2025-10-31 08:02:27 +0000

    add api_key to evaluator

--- evaluators/autopresent/reference_free_eval.py
+++ evaluators/autopresent/reference_free_eval.py
@@ -4,10 +4,10 @@ import pptx
 import base64
 import argparse
 from PIL import Image
+from openai import OpenAI
+from _api_keys import OPENAI_API_KEY
 
-import openai
-openai.api_key = os.environ["OPENAI_API_KEY"]
-client = openai.OpenAI()
+client = OpenAI(api_key=OPENAI_API_KEY)
 
 INSTRUCTION = """Please evaluate the slide based on the following criteria:
 {}
