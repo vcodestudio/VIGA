225422a26790e5e51da26da83d873865d8c87724
Shaofeng Yin <shaofengyin19260817@slurm-login-0.slurm-login.tenant-slurm.svc.cluster.local>
2025-12-05 10:05:36 +0000

    update submodules

---
+++ utils/path.py
@@ -0,0 +1,14 @@
+path_to_cmd = {
+    "tools/exec_blender.py": "/mnt/home/shaofengyin19260817/anaconda3/envs/blender/bin/python",
+    "tools/exec_slides.py": "/mnt/home/shaofengyin19260817/anaconda3/envs/pptx/bin/python",
+    "tools/exec_html.py": "/mnt/home/shaofengyin19260817/anaconda3/envs/chrome/bin/python",
+    "tools/generator_base.py": "/mnt/home/shaofengyin19260817/anaconda3/envs/agent/bin/python",
+    "tools/initialize_plan.py": "/mnt/home/shaofengyin19260817/anaconda3/envs/agent/bin/python",
+    "tools/investigator.py": "/mnt/home/shaofengyin19260817/anaconda3/envs/blender/bin/python",
+    "tools/meshy.py": "/mnt/home/shaofengyin19260817/anaconda3/envs/agent/bin/python",
+    "tools/verifier_base.py": "/mnt/home/shaofengyin19260817/anaconda3/envs/agent/bin/python",
+    "tools/undo.py": "/mnt/home/shaofengyin19260817/anaconda3/envs/agent/bin/python",
+    "tools/sam.py": "/mnt/home/shaofengyin19260817/anaconda3/envs/sam3d-objects/bin/python",
+    "tools/sam3_worker.py": "/mnt/home/shaofengyin19260817/anaconda3/envs/sam3/bin/python",
+    "tools/sam3d_worker.py": "/mnt/home/shaofengyin19260817/anaconda3/envs/sam3d-objects/bin/python",
+}
